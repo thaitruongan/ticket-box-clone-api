81d51faa8d9d24c9c9259dc10685e23aba439cc1
thaitruongan <thaitruonganlxag91@gmail.com>
2021-12-06 09:22:45 +0700

    server initialization

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const fileUpload = require('express-fileupload')
+const morgan = require('morgan');
+
+
+const app = express()
+app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+  useTempFiles: true
+}))
+
+// Connecting Routes
+
+app.get('/api',(req,res)=>{
+    res.send("hihi")
+})
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT)
+})
