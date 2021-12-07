e5e6b9d9ad1646d94cc0a61e7d01395e3f3a2641
pntn070599@gmail.com <pntn070599@gmail.com>
2021-12-07 10:14:27 +0700

    define user-model

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const path = require('path')
-const fileUpload = require('express-fileupload')
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
 
-
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors())
-app.use(cookieParser())
-app.use(fileUpload({
-  useTempFiles: true
-}))
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//db connection
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://pntn070599:JTuZ9VwW7XYHUpq@ticket-box-clone.jzdht.mongodb.net/dev?retryWrites=true&w=majority"
+  );
+
+  console.log("Db connected");
+}
 
 // Connecting Routes
 
-app.get('/api',(req,res)=>{
-    res.send("hihi")
-})
+app.get("/api", (req, res) => {
+  res.send("hihi");
+});
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res)=>{
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
---
+++ models/user-model.js
@@ -0,0 +1,110 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  phoneNumber: {
+    type: String,
+    unique: true,
+    match: [
+      /([\+84|84|0]+(3|5|7|8|9|1[2|6|8|9]))+([0-9]{8})\b/,
+      "Phone number is not valid",
+    ],
+    required: [true, "Phone number is required"],
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Email not valid",
+    ],
+  },
+
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    maxlength: 50,
+  },
+
+  birth: {
+    type: Date,
+    required: [true, "Birth date is required"],
+  },
+
+  sex: {
+    type: String,
+    default: "male",
+    enum: ["male", "female"],
+  },
+
+  avatar: {
+    type: String,
+    default: "default.png",
+  },
+
+  permission: {
+    type: [Schema.Types.ObjectId],
+    default: [],
+  },
+
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+
+  updatedAt: {
+    type: Date,
+    default: null,
+  },
+
+  updateBy: {
+    type: Schema.Types.ObjectId,
+    default: null,
+  },
+
+  version: {
+    type: Number,
+    default: 0,
+  },
+
+  oldVersion: {
+    type: [],
+    default: [],
+  },
+
+  facebook: {
+    type: {},
+    default: {},
+  },
+
+  google: {
+    type: {},
+    default: {},
+  },
+
+  activityLogs: {
+    type: [
+      {
+        time: Date,
+        log: String,
+      },
+    ],
+
+    default: [
+      {
+        time: new Date(),
+        log: "Account was created",
+      },
+    ],
+  },
+
+  activate: {
+    type: String,
+    default: "Created",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
