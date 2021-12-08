c5fbcb818464b27552682d3697d7370be6fead26
thaitruongan <thaitruonganlxag91@gmail.com>
2021-12-08 10:02:25 +0700

    fix banner-routes

--- routes/banner-routes.js
+++ routes/banner-routes.js
@@ -1,10 +1,10 @@
-const router = require("express").Router();
-// const postController = require("../controllers/banner-controllers");
+const router = require('express').Router()
+const postController = require("../controllers/banner-controllers");
 
-// router.post('/', postController.create);
-// router.put('/:id',postController.update);
-// router.delete('/:id',postController.delete);
-// router.get('/:id',postController.getById);
-// router.get('/',postController.list);
+router.post('/', postController.create);
+router.put('/:id',postController.update);
+router.delete('/:id',postController.delete);
+router.get('/:id',postController.getById);
+router.get('/',postController.list);
 
-module.exports = router;
+module.exports = router
