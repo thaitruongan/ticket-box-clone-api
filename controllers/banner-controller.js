35d08ecc3635e9740bd93e29996c6b8a485c109b
pntn070599@gmail.com <pntn070599@gmail.com>
2021-12-09 11:14:04 +0700

    banner-controller lookup film when List

--- controllers/banner-controller.js
+++ controllers/banner-controller.js
@@ -92,11 +92,19 @@ const bannerController = {
         return res.status(404).send({
           message: "Banner Not Found!",
         });
+      } else {
+        try {
+          await banner.delete();
+          res.status(200).json({
+            message: "Success! Post has been deleted",
+          });
+        } catch (err) {
+          res.status(500).json({
+            message: "Failed",
+            error: err,
+          });
+        }
       }
-      await banner.delete();
-      res.status(200).json({
-        message: "Success! Post has been deleted",
-      });
     } catch (err) {
       res.status(400).json({
         message: "Failed",
